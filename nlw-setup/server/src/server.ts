fbb7adbee2de4ed34efc1adec6973f26dea3f84e
Lucassec1 <lucasemmanuelcs@gmail.com>
2023-01-19 12:01:05 -0300

    feat: criando algumas rotas do back end

--- nlw-setup/server/src/server.ts
+++ nlw-setup/server/src/server.ts
@@ -1,17 +1,11 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/", async () => {
-    const habbits = await prisma.habit.findMany()
-
-    return habbits;
-});
+app.register(appRoutes);
 
 app.listen({
     port: 3333,
